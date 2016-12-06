5062f4bcb2fcf08a16b6d121d07a4efeab136f88
Keerush <keerush@live.com>
2016-12-06 01:46:24 -0500

    Revert "Changed bot token."
    This reverts commit 64be37dddffc2693b9a9ca6c6651abe2af5ccae8.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     "directory": __dirname,
     "emoji_location": __dirname + './public/res/emojis',
-    "botToken": "MjM2MjAwOTU1NjY4OTg3OTA0.CyfugQ.QBIZd0vd0O9E2dLWGdRLL4_NtAA"
+    "botToken": "MjU1NTUzMDg5MjI5MDI5Mzc2.CyfVrg.GezvzdFXjqOAtC8r0-BmlBPqLP8"
 }

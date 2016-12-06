5c70e38ae651af1f96b5d5cc63c07d7190f0df88
Keerush <keerush@live.com>
2016-12-06 01:21:19 -0500

    Created config file.

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "directory": __dirname,
+    "emoji_location": __dirname + './public/res/emojis',
+    "botToken": "MjU1NTUzMDg5MjI5MDI5Mzc2.CyfVrg.GezvzdFXjqOAtC8r0-BmlBPqLP8"
+}

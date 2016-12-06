cbdad0fec8c682dd55c3f11da9df5a2f40d35961
Keerush <keerush@live.com>
2016-12-06 00:46:43 -0500

    Setup ports using express.

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-var Discord = require('discordie')
+const Discord = require('discordie')
+const express = require('express')
 
 const Events = Discord.Events
 const client = new Discord()
 
+let app = express()
+
 client.connect({
-	token: 'MjU1NTUzMDg5MjI5MDI5Mzc2.CyfVrg.GezvzdFXjqOAtC8r0-BmlBPqLP8'
+    token: 'MjU1NTUzMDg5MjI5MDI5Mzc2.CyfVrg.GezvzdFXjqOAtC8r0-BmlBPqLP8'
 })
 
 client.Dispatcher.on(Events.GATEWAY_READY, e => {
-	console.log('Connected as: ' + client.User.username)
+    console.log('Connected as: ' + client.User.username)
 })
 
 client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
-	if (e.message.content == "ping")
-		e.message.channel.sendMessage("pong")
+    if (e.message.content == "ping")
+        e.message.channel.sendMessage("pong")
+})
+
+app.set('port', (process.env.PORT || 8080))
+
+app.get('/', function(request, response) {
+    response.send('Bloop!')
+})
+
+app.listen(app.get('port'), function() {
+    console.log("blooperbot is running at:" + app.get('port'))
 })

69637e0597e91fe086fad47f2259b760a8503449
Keerush <keerush@live.com>
2016-12-06 21:59:52 -0500

    Changed token.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     "directory": __dirname,
     "emojiLocation": __dirname + '/public/res/emojis',
-    "botToken": "MjU1NTUzMDg5MjI5MDI5Mzc2.CyfVrg.GezvzdFXjqOAtC8r0-BmlBPqLP8"
+    "botToken": "MjM2MjAwOTU1NjY4OTg3OTA0.CykMQQ.7f1BcH37zWRqM5HEu3NwyPfokGE"
 }

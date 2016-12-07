7aca5591fa5ec58386d9c0c8a89816d376fe7268
Keerush <keerush@live.com>
2016-12-06 21:56:10 -0500

    Fix api call.

--- public/index.js
+++ public/index.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
     response.send('Bloop!')
 })
 
-app.get('./wakemydyno.txt', (request, response) => {
+app.get('/wakemydyno.txt', (request, response) => {
   response.send('');
 })
 

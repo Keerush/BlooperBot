e5801a92fb814742152d5f50a612e267731b9dfb
Keerush <keerush@live.com>
2016-12-06 23:33:09 -0500

    Stored bot token into environment variables.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     "directory": __dirname,
     "emojiLocation": __dirname + '/public/res/emojis',
-    "botToken": "MjM2MjAwOTU1NjY4OTg3OTA0.CykMQQ.7f1BcH37zWRqM5HEu3NwyPfokGE"
+    "botToken": process.env.bot_token
 }

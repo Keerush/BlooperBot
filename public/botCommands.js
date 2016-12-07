ad0d6c68f154ab025ae98c7ee745ae270c4d0c1a
Keerush <keerush@live.com>
2016-12-06 23:11:33 -0500

    Added cats command.

--- public/botCommands.js
+++ public/botCommands.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const config = require('../config')
+const request = require('request')
+
 var images = []
 
 module.exports = {
@@ -10,6 +12,11 @@ module.exports = {
 
         if (content.toLowerCase() == 'bloop') {
             channel.sendMessage("bloop!")
+        } else if (content.toLowerCase() == 'cats') {
+            let uri = 'http://thecatapi.com/api/images/get?format=src&type=png';
+            request.head(uri, (err, res, body) => {
+              channel.sendMessage(res.request.uri.href);
+            })
         } else if (content.match(slashCmd)) {
             content.match(slashCmd).forEach((cmd) => {
                 // check if its an emoji cmd
